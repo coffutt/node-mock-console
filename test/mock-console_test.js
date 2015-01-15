2709d8e8fdef7cad93952e3a67e3a9260ac8de6e
Craig Offutt <craigoffutt@gmail.com>
2015-01-14 22:31:35 -0500

    Finished migrating to 2d approach, finished unit tests

--- test/mock-console_test.js
+++ test/mock-console_test.js
@@ -7,7 +7,7 @@ describe('mock-console tests', function () {
     var mockConsole, goodLines;
 
     beforeEach(function () {
-        mockConsole = require('../')(/^\*/);
+        mockConsole = require('../')(/^\*/, { width: 200, height: 200 });
         goodLines = [
             '* Write Line 1\n',
             '* Write Line 2\n',
@@ -26,80 +26,78 @@ describe('mock-console tests', function () {
     it('should clear the current line', function () {
         goodLines.forEach(process.stdout.write);
 
-        process.stdout.moveCursor(-5, -1);
+        process.stdout.moveCursor(-5, -2);
         process.stdout.clearLine();
 
-        var expected = [goodLines[0], goodLines[2]].join('\n');
+        var expected = [goodLines[0], goodLines[2]].join('');
         expect(mockConsole.getConsoleOutput()).to.equal(expected)
     });
-    //
-    // it('handles simulating window resizing', function () {
-    //     expect(process.stdout.windowSize()).to.deep.equal([100, 100]);
-    //     mockConsole.setWindowSize(200, 200);
-    //     expect(process.stdout.windowSize()).to.deep.equal([200, 200]);
-    // });
-    //
-    // describe('moving the cursor pos', function () {
-    //
-    //     var cases = [
-    //         {
-    //             msg: 'to the beggining of a line if currX + dx < 0',
-    //             dx: -15,
-    //             dy: 0,
-    //             expected: { x: 0, y: 3 }
-    //         },
-    //         {
-    //             msg: 'to the proper position if currX + dx > curr line length',
-    //             dx: 10,
-    //             dy: 0,
-    //             expected: { x: 23, y: 3 }
-    //         },
-    //         {
-    //             msg: 'to the first line if currY + dy < 0',
-    //             dx: 10,
-    //             dy: 0,
-    //             expected: { x: 13, y: 2 }
-    //         },
-    //         {
-    //             msg: 'to the last line if currY + dy > numLines',
-    //             dx: 0,
-    //             dy: 5,
-    //             expected: { x: 13, y: 2 }
-    //         },
-    //         {
-    //             msg: 'successfully to dx dy',
-    //             dx: -5,
-    //             dy: -1,
-    //             expected: { x: 8, y: 1 }
-    //         }
-    //     ];
-    //
-    //     cases.forEach(function (testCase) {
-    //         it('should move the cursor ' + testCase.msg, function () {
-    //             goodLines.forEach(process.stdout.write);
-    //
-    //             process.stdout.moveCursor(testCase.dx, testCase.dy);
-    //             expect(mockConsole._cursorPos()).to.deep.equal(testCase.expected);
-    //         });
-    //     });
-    // });
-    //
-    // it('should clear the screen downwards', function () {
-    //     goodLines.forEach(process.stdout.write);
-    //
-    //     process.stdout.moveCursor(10, -2);
-    //     process.stdout.clearScreenDown();
-    //
-    //     var actual = mockConsole.getConsoleOutput();
-    //     expect(actual).to.equal(goodLines[0]);
-    // });
-    //
-    // it('should reset stdout to its original function', function () {
-    //     mockConsole.resetStdout();
-    //
-    //     expect(mockConsole.getConsoleOutput()).to.equal('');
-    //     process.stdout.write('* abcdefg');
-    //     expect(mockConsole.getConsoleOutput()).to.equal('');
-    // });
+
+    it('handles simulating window resizing', function () {
+        expect(process.stdout.windowSize()).to.deep.equal([200, 200]);
+    });
+
+    describe('moving the cursor pos', function () {
+
+        var cases = [
+            {
+                msg: 'to the beggining of a line if currX + dx < 0',
+                dx: -15,
+                dy: 0,
+                expected: { x: 0, y: 3 }
+            },
+            {
+                msg: 'to the proper position if currX + dx > curr line length',
+                dx: 75,
+                dy: -1,
+                expected: { x: 75, y: 2 }
+            },
+            {
+                msg: 'to the first line if currY + dy < 0',
+                dx: 0,
+                dy: -5,
+                expected: { x: 0, y: 0 }
+            },
+            {
+                msg: 'to the proper line if currY + dy > numLines',
+                dx: 0,
+                dy: 5,
+                expected: { x: 0, y: 8 }
+            },
+            {
+                msg: 'successfully to dx dy',
+                dx: -5,
+                dy: -1,
+                expected: { x: 0, y: 2 }
+            }
+        ];
+
+        cases.forEach(function (testCase) {
+            it('should move the cursor ' + testCase.msg, function () {
+                goodLines.forEach(process.stdout.write);
+
+                process.stdout.moveCursor(testCase.dx, testCase.dy);
+                expect(mockConsole._cursor()).to.deep.equal(testCase.expected);
+            });
+        });
+    });
+
+    it('should clear the screen downwards', function () {
+        goodLines.forEach(process.stdout.write);
+
+        process.stdout.moveCursor(10, -3);
+        process.stdout.clearScreenDown();
+
+        var actual = mockConsole.getConsoleOutput();
+        expect(actual).to.equal(goodLines[0]);
+    });
+
+    it('should reset stdout to its original function', function () {
+        mockConsole.resetStdout();
+
+        expect(mockConsole.getConsoleOutput()).to.equal('');
+        process.stdout.write('* abcdefg');
+        expect(mockConsole.getConsoleOutput()).to.equal('');
+    });
 
 });
